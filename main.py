4b374b70f003d5738b3f2bf9e5e2ed1c75a75512
BeamINDU <b.beam.sitthan@gmail.com>
2024-02-28 11:57:38 +0700

    edit V2

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from pyautogui import *
+import win32api
+import win32con
+import cv2
+import numpy as np
 import pyautogui
 import time
 import keyboard
-import random
-import win32api, win32con
 
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+# หยุดเป็นเวลา 2 วินาที
 time.sleep(2)
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-
-#Color of center: (255, 219, 195)
+# โหลดภาพที่ต้องการหา
+template = cv2.imread('gold2.png')
+template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
 while keyboard.is_pressed('q') == False:
-    flag = 0
-    pic = pyautogui.screenshot(region=(584, 422, 751, 526))
+    # จับภาพหน้าจอในบริเวณที่กำหนด
+    screenshot = pyautogui.screenshot(region=(700, 500, 751, 700))
+    screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-    width, height = pic.size
+    # ทำ grayscale สำหรับทั้งภาพหน้าจอและภาพที่ต้องการหา
+    screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
 
-    for x in range(0, width, 5):
-        for y in range(0, height, 5):
+    # ค้นหาภาพที่ต้องการในภาพหน้าจอ
+    result = cv2.matchTemplate(screenshot_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-            r, g, b = pic.getpixel((x, y))
+    threshold = 0.7
 
-            if b == 58 and r == 253 and g == 233:
-                flag = 1
-                click(x+584, y+422)
-                time.sleep(0.05)
-                break
-
-        if flag == 1:
-            break
+    if max_val >= threshold:
+        # หากพบภาพที่ต้องการในภาพหน้าจอ
+        click(max_loc[0] + 584, max_loc[1] + 422)
+        time.sleep(0.05)
